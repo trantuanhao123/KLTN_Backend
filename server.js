d9ddb2d2752f725cdfe1755acd6483f7d244f9e7
Hao <trantuanhao308@gmail.com>
2025-08-09 18:50:43 +0700

    SRC DONE DAY 15

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-const { createServer } = require('node:http');//đã cài nodejs
+const express = require('express')
+const app = express()
+const port = 8080
 
-const hostname = '127.0.0.1';//localhost
-const port = 3000;
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
