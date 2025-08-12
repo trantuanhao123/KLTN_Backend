225f6233fe6b104e4bd6cbf8e4b5dbc7c752c1d5
Hao <trantuanhao308@gmail.com>
2025-08-12 14:33:06 +0700

    DONE STATIC FILE

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 const express = require('express') //common js
 require('dotenv').config()
 const app = express() //app express
-const port = process.env.PORT || 8080//port
-
+const port = process.env.PORT || 8080 //port
+const path = require('path') //path
 //config template engine 
-app.set('views','./src/views')
+app.set('views',path.join(__dirname,'views'))
 app.set('view engine', 'ejs')
+
+//config static files
+app.use(express.static(path.join(__dirname,'public')))
 
 //khai bao route
 app.get('/', (req, res) => {
