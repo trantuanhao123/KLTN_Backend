0644994657d3b80a71e3a3d9e8761aeb66fa57ae
Hao <trantuanhao308@gmail.com>
2025-08-16 16:33:14 +0700

    DONE DAY 30

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express') //common js
 const path = require('path')  //path
 const configViewEngine = require('./config/viewengine')
 const webRouter = require('./routes/web')
+const mysql = require('mysql2')
 
 const app = express() //app express
 const port = process.env.PORT || 8080 //port
@@ -14,6 +15,21 @@ configViewEngine(app)
 //khai bao route
 app.use('/',webRouter)
 
+//test connection 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: '3307', //default:3306
+  user: 'root',
+  password: '123456',
+  database: 'haotuhoc'
+})
+connection.query(
+  'SELECT * FROM Users',
+  function(err,result,fields){
+    console.log(">>> result= ",result)
+    console.log(">>> fields= ",fields)
+  }
+)
 app.listen(port,hostname, () => {
   console.log(`Example app listening on port ${port}`)
 })
