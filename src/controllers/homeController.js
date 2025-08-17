78669cd672808fb93bb0f0276ce0d149a28d0b26
Hao <trantuanhao308@gmail.com>
2025-08-17 13:37:29 +0700

    DONE DAY 31

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,18 @@
-const getHomePage = (req,res) => {
-    res.render('sample.ejs')
-}
+const connection = require("../config/database");
+
+const getHomePage = (req, res) => {
+  let users = [];
+  connection.query("SELECT * FROM Users", function (err, result) {
+    users = result;
+    console.log(">>> result home page =  ", result);
+    res.send(JSON.stringify(users));
+  });
+};
+
+const getHaoTuHoc = (req, res) => {
+  res.render("sample.ejs");
+};
 module.exports = {
-    getHomePage
-}
+  getHomePage,
+  getHaoTuHoc,
+};
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const {
-    getHomePage
+    getHomePage,
+    getHaoTuHoc
 }=require('../controllers/homeController')
 
 router.get('/',  getHomePage)
-
+router.get('/haotuhoc',getHaoTuHoc)
 module.exports = router
