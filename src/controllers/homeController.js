0b7f15e0294f91ec3f9644d42666cc58fb2e12f0
Hao <trantuanhao308@gmail.com>
2025-08-20 17:18:40 +0700

    RECOMMIT DAY 38

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -18,7 +18,7 @@ const postCreateUser = (req, res) => {
   let email = req.body.email;
   let name = req.body.name;
   let city = req.body.city;
-  console.log(">>>>email: ", email, ">>>>name: ", name, ">>>>city: ", city);
+  //console.log(">>>>email: ", email, ">>>>name: ", name, ">>>>city: ", city);
   connection.query(
     `INSERT INTO Users (email,name,city)
      VALUES (?,?,?)
