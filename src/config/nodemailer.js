36ce61e1da42d04b378224953b78301f0ca34ead
Phan Thuận Kiệt <kietphan5115@gmail.com>
2025-11-30 17:48:35 +0700

    delete Nodemailer

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -7,32 +7,3 @@
 //     pass: process.env.MAILER_PASSWORD,
 //   },
 // });
-
-// module.exports = transporter;
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,            
-  secure: false,          
-  auth: {
-    user: process.env.MAILER_USER,
-    pass: process.env.MAILER_PASSWORD,
-  },
-  tls: {
-    rejectUnauthorized: false 
-  },
-  connectionTimeout: 10000, 
-  greetingTimeout: 10000,   
-  socketTimeout: 10000,     
-});
-
-transporter.verify(function (error, success) {
-  if (error) {
-    console.error("Nodemailer Error:", error);
-  } else {
-    console.log("Server đã sẵn sàng gửi email (Port 587).");
-  }
-});
-
-module.exports = transporter;
